e1d6e582df06a0c672cf0fc81772a692e97f2f0e
sejinlim1107 <dlatpwls834@gmail.com>
2021-08-27 02:26:35 +0900

    class2

--- java/class2/src/Main1929.java
+++ java/class2/src/Main1929.java
@@ -5,24 +5,29 @@ public class Main1929 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         int M = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        boolean prime = false;
+        int [] isPrime = new int[N+1];
 
-        for(int i=M; i<=N; i++){
-            for(int j=2; j<=(int)Math.sqrt(i/2);j++){
-                if(i % j == 0){
-                    prime = false;
-                    break;
-                }
-                prime = true;
-            }
-            if(prime)
-                sb.append(i+"\n");
-            prime = true;
+        for(int i=2;i<=N;i++){
+            isPrime[i] = i;
         }
-        //System.out.println(sb);
+
+        for(int i=2;i<=(int)Math.sqrt(N);i++){
+            if(isPrime[i] == 0) continue;
+            for(int j=2*i;j<=N;j+=i){
+                isPrime[j] = 0;
+            }
+        }
+
+        for(int i=M;i<=N;i++){
+            if(isPrime[i] != 0){
+                sb.append(isPrime[i]+"\n");
+            }
+        }
+
+        System.out.println(sb);
     }
 }
---
+++ java/class2/src/Main2775.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main2775 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int num = 0;
+        int tmp = 0;
+
+        for(int i=0;i<T;i++){
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+
+            for(int j=0;j<=k;j++){
+                for(int z=1;z<=n;z++){
+                    if(j==0)
+                        num+=z;
+                    else {
+                        tmp += num+
+                        num+=tmp;
+                    }
+                }
+            }
+        }
+
+
+    }
+}
