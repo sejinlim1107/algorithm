bcba072debc9bac1df2cc67931d7bd5426b50725
sejinlim1107 <dlatpwls834@gmail.com>
2021-09-04 23:04:32 +0900

    class2 ing

--- java/class2/src/Main1920.java
+++ java/class2/src/Main1920.java
@@ -2,46 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main1920 {
+    /*
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        HashSet<String> set = new HashSet<String>();
+        Set<Integer> set = new HashSet<>();
         st = new StringTokenizer(br.readLine());
 
         for(int i=0;i<N;i++){
-            a[i] = Integer.parseInt(st.nextToken());
+            set.add(Integer.parseInt(st.nextToken()));
         }
-
-        Arrays.sort(a);
 
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
         for(int i=0;i<M;i++){
-            sb.append(binary_search(a,Integer.parseInt(st.nextToken()))).append("\n");
+            if(set.contains(Integer.parseInt(st.nextToken())))
+                sb.append("1\n");
+            else
+                sb.append("0\n");
+
         }
 
         System.out.println(sb);
     }
-    /*
-    public static int binary_search(int[] a, int target){
-        int start = 0;
-        int end = a.length-1;
-
-        while(start <= end){
-            int mid = (int)(start+end)/2;
-
-            if(a[mid] == target)
-                return 1;
-            else if(a[mid] < target)
-                start = mid+1;
-            else
-                end = mid-1;
-
-        }
-        return 0;
-    }
+         */
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -62,11 +49,13 @@ public class Main1920 {
         st = new StringTokenizer(br.readLine());
 
         for(int i=0;i<M;i++){
-            sb.append(binary_search(a,Integer.parseInt(st.nextToken()))).append("\n");
+            if(Arrays.binarySearch(a,Integer.parseInt(st.nextToken())) >= 0)
+                sb.append(1).append("\n");
+            else
+                sb.append(0).append("\n");
         }
 
         System.out.println(sb);
     }
-     */
 }
 
--- java/class2/src/Main2775.java
+++ java/class2/src/Main2775.java
@@ -4,40 +4,25 @@ import java.io.*;
 public class Main2775 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+        int human[][] = new int[15][15];
 
-        for(int i=0;i<T;i++) {
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
+        for(int i=1;i<15;i++) //0층 셋팅
+            human[0][i] = i;
 
-
-        }
-        /*
-        int num = 0;
-        int tmp = 0;
-
-        for(int i=0;i<T;i++){
-            st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            int n = Integer.parseInt(st.nextToken());
-
-            for(int j=0;j<=k;j++){
-                for(int z=1;z<=n;z++){
-                    if(j==0)
-                        num+=z;
-                    else {
-                        //tmp += num+
-                        //num+=tmp;
-                    }
-                }
+        for(int i=1;i<15;i++){
+            for(int j=1;j<15;j++){
+                human[i][j] = human[i][j-1]+human[i-1][j];
             }
         }
 
+        for(int i=0;i<T;i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-         */
-
+            sb.append(human[k][n]).append("\n");
+        }
+        System.out.println(sb);
     }
 }
