1762df9acef667071e293f2e9c99092f10cc342e
sejinlim1107 <tpwls834@naver.com>
2021-09-11 23:46:52 +0900

    boj

---
+++ java/class2/src/Main2108.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main2108 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int human[][] = new int[15][15];
+
+        for(int i=1;i<15;i++) //0층 셋팅
+            human[0][i] = i;
+
+        for(int i=1;i<15;i++){
+            for(int j=1;j<15;j++){
+                human[i][j] = human[i][j-1]+human[i-1][j];
+            }
+        }
+
+        for(int i=0;i<T;i++) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(human[k][n]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
