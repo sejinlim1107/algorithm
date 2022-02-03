2bfff2ef4c44b6ead315cf202c636fe3147131c2
sejinlim1107 <dlatpwls834@gmail.com>
2022-02-03 20:22:14 +0900

    class3

--- java/class3/src/Main2579.java
+++ java/class3/src/Main2579.java
@@ -5,20 +5,17 @@ public class Main2579 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        int[] scores = new int[num+1];
-        int[] dp = new int[num+1];
+        int[] scores = new int[num+2];
+        int[] dp = new int[num+2];
 
         for(int i=1;i<num+1;i++){
             scores[i] = Integer.parseInt(br.readLine());
         }
-
         dp[1] = scores[1];
-        dp[2] = scores[2];
+        dp[2] = scores[1]+scores[2];
 
-        for(int i=3;i<num+1;i++){
-            dp[i] = Math.min(dp[i-2]+scores[i],dp[i-1]);
-            System.out.println(i+" "+dp[i]);
-        }
+        for(int i=3;i<num+1;i++)
+            dp[i] = Math.max(dp[i-2],dp[i-3]+scores[i-1])+scores[i];
         System.out.println(dp[num]);
     }
 }
