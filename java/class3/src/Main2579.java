0a54a2a19087ea3fde7849527dc2013f78a528cb
sejinlim1107 <dlatpwls834@gmail.com>
2022-02-03 18:46:57 +0900

    class3

--- java/class3/src/Main2579.java
+++ java/class3/src/Main2579.java
@@ -7,14 +7,18 @@ public class Main2579 {
         int num = Integer.parseInt(br.readLine());
         int[] scores = new int[num+1];
         int[] dp = new int[num+1];
-        dp[1] = scores[1];
-        dp[2] = scores[2];
-        for(int i=0;i<num+1;i++){
+
+        for(int i=1;i<num+1;i++){
             scores[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=3;i<num+1;i++){ // 범위 주의
-            dp[i] = Math.min(dp[i-1]+scores[i+1],dp[i-1]+scores[i]);
+        dp[1] = scores[1];
+        dp[2] = scores[2];
+
+        for(int i=3;i<num+1;i++){
+            dp[i] = Math.min(dp[i-2]+scores[i],dp[i-1]);
+            System.out.println(i+" "+dp[i]);
         }
+        System.out.println(dp[num]);
     }
 }
