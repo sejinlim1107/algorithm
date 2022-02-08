b4a5966b83b5c973445ccdb9615be29aa586a85b
sejinlim1107 <tpwls834@naver.com>
2022-02-08 23:22:33 +0900

    boj

---
+++ java/random/src/Main5339.java
@@ -0,0 +1,14 @@
+public class Main5339 {
+    public static void main(String[] args) {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
