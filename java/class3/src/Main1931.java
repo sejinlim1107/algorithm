c24bc378653a7dc76102fa08feae159eaeb36364
sejinlim1107 <dlatpwls834@gmail.com>
2022-02-10 15:52:07 +0900

    class3

--- java/class3/src/Main1931.java
+++ java/class3/src/Main1931.java
@@ -6,34 +6,33 @@ public class Main1931 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        HashMap<Integer,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>(); // 시간차
-        int answer = 0;
         int n = Integer.parseInt(br.readLine());
+        int time[][] = new int[n][2];
+        int answer = 0;
         for(int i=0;i<n;i++){
             st  = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            map1.put(a,b);
-            map2.put(a,b-a);
+            time[i][0] = a;
+            time[i][1] = b;
         }
-        Object[] keys = map1.keySet().toArray();
-        Arrays.sort(keys);
-        List<Entry<Integer, Integer>> list = new ArrayList<Entry<Integer, Integer>>(map2.entrySet());
-        Collections.sort(list, new Comparator<Entry<Integer, Integer>>() {
-            public int compare(Entry<Integer, Integer> o1, Entry<Integer, Integer> o2) {
-                // 오름차순 정렬
-                return o1.getValue().compareTo(o2.getValue());
+        Arrays.sort(time, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1])
+                    return o1[0]-o2[0];
+                return o1[1]-o2[1];
             }
         });
-
-        for(Integer key : map1.keySet()){
-            System.out.println(key+" "+map1.get(key));
-
+        int last = -1;
+        for(int i=0;i<n;i++) {
+            int start = time[i][0];
+            int end = time[i][1];
+            if(start >= last){
+                last = end;
+                answer++;
+            }
         }
-
-        for(Entry<Integer, Integer> entry : list) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        System.out.println(answer);
     }
 }
