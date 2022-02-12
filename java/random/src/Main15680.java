d1e7ba4a1ca3928f75b4b5653f5cc40362c2e759
sejinlim1107 <tpwls834@naver.com>
2022-02-12 23:07:53 +0900

    boj

---
+++ java/random/src/Main15680.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main15680 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n==1){
+            System.out.println("Leading the Way to the Future");
+        }
+        else{
+            System.out.println("YONSEI");
+        }
+    }
+}
