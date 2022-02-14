0f2add6933e60ffceda91dee4787cc3448b4c5f0
sejinlim1107 <dlatpwls834@gmail.com>
2022-02-14 22:28:15 +0900

    boj

--- java/class3/src/Main5525.java
+++ java/class3/src/Main5525.java
@@ -4,31 +4,19 @@ import java.io.*;
 public class Main5525 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine()); // O가 N개
         int len = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        int total = 0;
-        int tmp = 0;
-        int preCnt = 0;
+        int cnt = 0; // IO 개수
+        int total = 0;;
         String s = br.readLine();
         for(int i=0;i<len;i++){
-            if(cnt%2==0 && s.charAt(i)=='I'){ // I
-                cnt++;
-            }
-            else if(cnt%2==1 && s.charAt(i)=='O') { // O
-                cnt++;
-            }
-            if(preCnt == cnt) // 연속해서 cnt가 변하지 않으면
-                cnt = 0;
-            if(cnt==1)
-                tmp = i;
-            preCnt = cnt;
-            if(cnt == 2*n+1) {
-                cnt = 0;
-                i = tmp + 2;
-                total++;
+            if(s.charAt(i) == 'I'){
+                if(i+1<len && s.charAt(i+1)=='O'){
+                    cnt++;
+                    i++;
+                }
             }
         }
-        System.out.println(total);
+        System.out.println(cnt);
     }
 }
