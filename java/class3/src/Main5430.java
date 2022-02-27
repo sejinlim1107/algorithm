be8b0a15d4423b0c7dd390525ea8361cd8482399
sejinlim1107 <dlatpwls834@gmail.com>
2022-02-27 23:51:24 +0900

    boj

---
+++ java/class3/src/Main5430.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main5430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            String f = br.readLine(); // 함수
+            int n = Integer.parseInt(br.readLine());
+            if(n==0){ // 빈 배열 일 때
+                if(f.contains("D")){
+                    sb.append("error").append("\n");
+                }
+                continue;
+            }
+            boolean mode = true; // true면 앞에서부터, false면 뒤에서부터
+            boolean error = false;
+            String arr = br.readLine();
+            Deque<Integer> deque = new LinkedList<>();
+            arr = arr.substring(1,arr.length()-1); // [] 뺀 문자열
+            String[] num = arr.split(",");
+            for(int i=0;i<n;i++){
+                deque.add(Integer.parseInt(num[i]));
+            }
+            for(int i=0;i<f.length();i++){
+                if(f.charAt(i)=='R'){
+                    mode = !mode;
+                }
+                else{ // 'D'
+                    if(!deque.isEmpty()) {
+                        if (mode) {
+                            deque.pollFirst();
+                        }
+                        else{
+                            deque.pollLast();
+                        }
+                    }
+                    else {
+                        error = true;
+                        break;
+                    }
+                }
+            }
+            if(error)
+                sb.append("error").append("\n");
+            else{
+                sb.append("[");
+                if(mode) {
+                    for (Iterator<Integer> it = deque.iterator(); it.hasNext();) {
+                        sb.append(it.next() + ",");
+                    }
+                }
+                else{
+                    for (Iterator<Integer> it = deque.descendingIterator(); it.hasNext();) {
+                        sb.append(it.next() + ",");
+                    }
+                }
+                sb.deleteCharAt(sb.length()-1); // , 없애기
+                sb.append("]").append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
---
+++ java/random/src/Main16394.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main16394 {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        System.out.println(year-1946);
+    }
+}
