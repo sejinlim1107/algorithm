671ed0b8c14fd49827baeda62edd3efdb30dfc9c
sejinlim1107 <dlatpwls834@gmail.com>
2022-03-02 19:45:34 +0900

    boj

---
+++ java/random/src/Main1550.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main1550 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine(),16);
+        System.out.println(n);
+    }
+}
