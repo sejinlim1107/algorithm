a7a56b651fac0bb6c7d7010de7b18351c594da7a
sejinlim1107 <dlatpwls834@gmail.com>
2022-03-03 19:55:02 +0900

    boj

---
+++ java/class3/src/Main1107.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class Main1107 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int target = Integer.parseInt(s);
+        int now = 100;
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] error = new int[m];
+        int cnt = 0;
+        for(int i=0;i<m;i++){
+            error[i] = Integer.parseInt(st.nextToken());
+        }
+        while(now != target){
+
+        }
+    }
+}
---
+++ java/random/src/Main16430.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main16430 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(b-a+" "+b);
+    }
+}
