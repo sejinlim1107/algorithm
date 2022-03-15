a52005072c478dc6645365437a1244f467259f72
sejinlim1107 <dlatpwls834@gmail.com>
2022-03-15 16:20:57 +0900

    boj

---
+++ java/random/src/Main24262.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main24262 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        sb.append(1);
+        sb.append("\n");
+        sb.append(0);
+        System.out.println(sb);
+    }
+}
