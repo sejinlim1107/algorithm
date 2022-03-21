679561d9d2f37ebcc5e2b0dc04f06ce5577b33fa
sejinlim1107 <tpwls834@naver.com>
2022-03-21 23:02:56 +0900

    boj

---
+++ java/random/src/Main2420.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main2420 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long n = Long.parseLong(st.nextToken());
+        long m = Long.parseLong(st.nextToken());
+        System.out.println(Math.abs(n-m));
+    }
+}
