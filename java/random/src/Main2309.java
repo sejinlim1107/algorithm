7652994af75a52f0a58b664bf5c540b765988c6e
sejinlim1107 <tpwls834@naver.com>
2022-03-22 23:04:08 +0900

    boj

--- java/random/src/Main2309.java
+++ java/random/src/Main2309.java
@@ -6,15 +6,27 @@ public class Main2309 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int[] tall = new int[9];
+        int sum = 0;
+        int p1, p2;
         for(int i=0;i<9;i++){
             tall[i] = Integer.parseInt(br.readLine());
+            sum+=tall[i];
         }
         Arrays.sort(tall);
-        int sum = 0;
-        for(int i=0;i<7;i++){
-            if(sum>100)
-                continue;
-            sum+=tall[i];
+        for(int i=0;i<9;i++){
+            p1 = tall[i];
+            for(int j=i+1;j<9;j++){
+                p2 = tall[j];
+                if(sum-p1-p2 == 100){
+                    for(int k=0;k<9;k++){
+                        if(k==i || k==j)
+                            continue;
+                        sb.append(tall[k]).append("\n");
+                    }
+                    System.out.println(sb);
+                    return;
+                }
+            }
         }
     }
 }
