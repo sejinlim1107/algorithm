18107bc9e5755022d4543c81361c140ff3b8bbf9
sejinlim1107 <dlatpwls834@gmail.com>
2022-04-02 21:55:11 +0900

    class3

--- java/class3/src/Main11403.java
+++ java/class3/src/Main11403.java
@@ -2,17 +2,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main11403 {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         int[][] mat = new int[n][n];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) { // 입력받아서 넣기
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<n;j++){
+            for (int j = 0; j < n; j++) {
                 mat[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        for (int k = 0; k < n; k++) { // 거쳐갈 아이 설정
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (mat[i][k] == 1 && mat[k][j] == 1) {
+                        mat[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append(mat[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
