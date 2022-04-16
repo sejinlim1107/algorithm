72dd4357693725950880f0476ab83d047063d4df
sejinlim1107 <tpwls834@naver.com>
2022-04-16 23:48:40 +0900

    boj

--- java/random/src/Main23972.java
+++ java/random/src/Main23972.java
@@ -7,6 +7,13 @@ public class Main23972 {
         StringTokenizer st = new StringTokenizer(br.readLine());
         long k = Long.parseLong(st.nextToken());
         long n = Long.parseLong(st.nextToken());
-
+        if(n==1){
+            System.out.println(-1);
+            return;
+        }
+        long answer = n*k/(n-1);
+        if((n*k)%(n-1)!=0)
+            answer++;
+        System.out.println(answer);
     }
 }
