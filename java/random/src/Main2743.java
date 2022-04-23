ab978388f4edffa2ecfbd56c2aa40679c00c312f
sejinlim1107 <dlatpwls834@gmail.com>
2022-04-23 14:50:39 +0900

    boj

---
+++ java/random/src/Main2743.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}
