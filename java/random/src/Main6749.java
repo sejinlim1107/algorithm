0f05c7ddc092b9fc9291d17ab6eb856cc8dc3d3e
sejinlim1107 <dlatpwls834@gmail.com>
2022-04-29 14:17:21 +0900

    boj

---
+++ java/random/src/Main6749.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main6749 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        System.out.println(m+m-y);
+    }
+}
