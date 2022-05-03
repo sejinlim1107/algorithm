4bb3360f47217e119ede46765a5cfbdc6ca14a9b
sejinlim1107 <dlatpwls834@gmail.com>
2022-05-03 21:40:15 +0900

    boj

---
+++ java/random/src/Main23825.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main23825 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(n/2,m/2));
+    }
+}
