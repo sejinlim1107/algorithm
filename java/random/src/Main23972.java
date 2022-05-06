e30f0dc7c7098f2fae1d95607e1be2bb81e6f206
sejinlim1107 <tpwls834@naver.com>
2022-05-06 12:38:37 +0900

    boj

--- java/random/src/Main23972.java
+++ java/random/src/Main23972.java
@@ -5,13 +5,13 @@ public class Main23972 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long k = Long.parseLong(st.nextToken());
-        long n = Long.parseLong(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         if(n==1){
             System.out.println(-1);
             return;
         }
-        long answer = n*k/(n-1);
+        long answer = (long)n*k/(n-1);
         if((n*k)%(n-1)!=0)
             answer++;
         System.out.println(answer);
