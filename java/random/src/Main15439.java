20338f55ab835b6cd22f898b0668d6ed597eff6e
sejinlim1107 <tpwls834@naver.com>
2022-05-07 13:06:04 +0900

    boj

---
+++ java/random/src/Main15439.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(n*(n-1));
+    }
+}
