102e7b29e668f5ba696da0ad00fb49d83cc9f526
sejinlim1107 <dlatpwls834@gmail.com>
2022-05-11 20:01:00 +0900

    boj

---
+++ java/class3/src/Main7576.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main7576 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        while()
+    }
+}
---
+++ java/random/src/Main23234.java
@@ -0,0 +1,5 @@
+public class Main23234 {
+    public static void main(String[] args) {
+        System.out.println("The world says hello!");
+    }
+}
