c8fa886a44fc42fceb80111181990d2c11168e68
sejinlim1107 <dlatpwls834@gmail.com>
2022-05-15 23:47:16 +0900

    boj

---
+++ java/random/src/Main25191.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main25191 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(Math.min(n,a/2+b));
+    }
+}
